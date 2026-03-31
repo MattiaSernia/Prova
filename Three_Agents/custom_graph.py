819c800a364da2fead16c73875a7921a3fbd3355
Mattia Sernia <msernia@ptb-04017402.inria.fr>
2026-03-31 14:24:24 +0200

    mancava il self

--- Three_Agents/custom_graph.py
+++ Three_Agents/custom_graph.py
@@ -41,7 +41,7 @@ class Custom_Graph:
             mxg=messages[i]
             error=False
             if mxg.role=="default":
-                URIagent=URIRef(self.nodeUri + clean_uri(mxg.node))
+                URIagent=URIRef(self.nodeUri + self.clean_uri(mxg.node))
                 if (URIagent, RDF.type, self.NS.Agent) not in self.graph:
                     self.graph.add((URIagent, RDF.type, self.NS.Agent))
                 if mxg.text in self.dict:
