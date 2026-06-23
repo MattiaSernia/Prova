143de785de6c41eb0fe387ec787127714294f9f2
Mattia Sernia <msernia@fsophia>
2026-06-23 09:58:56 +0200

    experiments: all

---
+++ kg_claude/run_all.sh
@@ -0,0 +1,38 @@
+python3 main.py --cft belval --c-oap --extractor phi4 --kg-format json
+python3 main.py --cft belval --c-oap --extractor phi4 --kg-format json --no-text
+python3 main.py --cft belval --c-oap --extractor phi4 --kg-format turtle-light
+python3 main.py --cft belval --c-oap --extractor phi4 --kg-format turtle-light --no-text
+python3 main.py --cft belval --c-oap --extractor phi4 --kg-format json --no-schema
+python3 main.py --cft belval --c-oap --extractor phi4 --kg-format json --no-schema --no-text
+python3 main.py --cft belval --c-oap --extractor phi4 --kg-format turtle-light --no-schema
+python3 main.py --cft belval --c-oap --extractor phi4 --kg-format turtle-light --no-schema --no-text
+python3 main.py --cft belval --c-oap --extractor llama --kg-format json
+python3 main.py --cft belval --c-oap --extractor llama --kg-format json --no-text
+python3 main.py --cft belval --c-oap --extractor llama --kg-format turtle-light
+python3 main.py --cft belval --c-oap --extractor llama --kg-format turtle-light --no-text
+
+python3 main.py --cft chrb --c-oap --extractor phi4 --kg-format json
+python3 main.py --cft chrb --c-oap --extractor phi4 --kg-format json --no-text
+python3 main.py --cft chrb --c-oap --extractor phi4 --kg-format turtle-light
+python3 main.py --cft chrb --c-oap --extractor phi4 --kg-format turtle-light --no-text
+python3 main.py --cft chrb --c-oap --extractor phi4 --kg-format json --no-schema
+python3 main.py --cft chrb --c-oap --extractor phi4 --kg-format json --no-schema --no-text
+python3 main.py --cft chrb --c-oap --extractor phi4 --kg-format turtle-light --no-schema
+python3 main.py --cft chrb --c-oap --extractor phi4 --kg-format turtle-light --no-schema --no-text
+python3 main.py --cft chrb --c-oap --extractor llama --kg-format json
+python3 main.py --cft chrb --c-oap --extractor llama --kg-format json --no-text
+python3 main.py --cft chrb --c-oap --extractor llama --kg-format turtle-light
+python3 main.py --cft chrb --c-oap --extractor llama --kg-format turtle-light --no-text
+
+python3 main.py --cft cabinet --c-oap --extractor phi4 --kg-format json
+python3 main.py --cft cabinet --c-oap --extractor phi4 --kg-format json --no-text
+python3 main.py --cft cabinet --c-oap --extractor phi4 --kg-format turtle-light
+python3 main.py --cft cabinet --c-oap --extractor phi4 --kg-format turtle-light --no-text
+python3 main.py --cft cabinet --c-oap --extractor phi4 --kg-format json --no-schema
+python3 main.py --cft cabinet --c-oap --extractor phi4 --kg-format json --no-schema --no-text
+python3 main.py --cft cabinet --c-oap --extractor phi4 --kg-format turtle-light --no-schema
+python3 main.py --cft cabinet --c-oap --extractor phi4 --kg-format turtle-light --no-schema --no-text
+python3 main.py --cft cabinet --c-oap --extractor llama --kg-format json
+python3 main.py --cft cabinet --c-oap --extractor llama --kg-format json --no-text
+python3 main.py --cft cabinet --c-oap --extractor llama --kg-format turtle-light
+python3 main.py --cft cabinet --c-oap --extractor llama --kg-format turtle-light --no-text
