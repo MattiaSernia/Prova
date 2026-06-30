b2f09a9192d66aa46651ffc5043653941d94bc5a
Mattia Sernia <msernia@ptb-04017402.inria.fr>
2026-06-30 10:03:13 +0200

    fix:phi4

--- kg_claude/run_all.sh
+++ kg_claude/run_all.sh
@@ -58,6 +58,6 @@ python3 main.py --cft cabinet --c-oap --extractor phi4 --kg-format yaml-ld --no-
 #python3 main.py --cft cabinet --c-oap --extractor llama --kg-format yaml-ld
 #python3 main.py --cft cabinet --c-oap --extractor llama --kg-format yaml-ld --no-text
 
-python3 main.py --cft cabinet --c-null
-python3 main.py --cft chrb --c-null
-python3 main.py --cft belval --c-null
+#python3 main.py --cft cabinet --c-null
+#python3 main.py --cft chrb --c-null
+#python3 main.py --cft belval --c-null
